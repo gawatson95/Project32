d02492445f0d8b67571afccea9d7638e68094ccc
Grant Watson <gaw922@gmail.com>
2022-11-08 16:50:29 -0600

    Project32

--- Project32/ViewController.swift
+++ Project32/ViewController.swift
@@ -5,15 +5,26 @@
 //  Created by Grant Watson on 11/7/22.
 //
 
-import UIKit
+import CoreSpotlight
+import MobileCoreServices
 import SafariServices
+import UIKit
 
 class ViewController: UITableViewController {
     
     var projects = [[String]]()
+    var favorites = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
         
         projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
         projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
@@ -36,7 +47,67 @@ class ViewController: UITableViewController {
         config.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
         cell.contentConfiguration = config
         
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favorites.firstIndex(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func index(item: Int) {
+        let project = projects[item]
+        
+        let attributeSet = CSSearchableItemAttributeSet(contentType: UTType.text)
+        attributeSet.title = project[0]
+        attributeSet.contentDescription = project[1]
+        
+        let item = CSSearchableItem(uniqueIdentifier: "\(item)", domainIdentifier: "com.hackingwithswift", attributeSet: attributeSet)
+        item.expirationDate = .distantFuture
+        
+        CSSearchableIndex.default().indexSearchableItems([item]) { error in
+            if let error = error {
+                print("Indexing error: \(error.localizedDescription)")
+            } else {
+                print("Index successful.")
+            }
+        }
+    }
+    
+    func deindex(item: Int) {
+        CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ["\(item)"]) { error in
+            if let error = error {
+                print("Deindexing error: \(error.localizedDescription)")
+            } else {
+                print("Deindex successful.")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
